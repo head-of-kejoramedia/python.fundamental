e3ac4f4758ac9220a653e687e6fa73a25196a613
Asus-Hasan <kejoramedia@gmail.com>
2022-12-06 21:49:04 +0700

    perulangan dengan for

--- dasar_sintaksis_perulangan.py
+++ dasar_sintaksis_perulangan.py
@@ -10,3 +10,5 @@ print(f'Jumlah buku yg sudah dibaca {jumlah_buku_yg_sudah_dibaca}')
 for jumlah_buku_yg_sudah_dibaca in range (1, jumlah_total_buku+1):
     print(f'Membaca buku ke {jumlah_buku_yg_sudah_dibaca}')
 print(f'Jumlah buku yg sudah dibaca {jumlah_buku_yg_sudah_dibaca}')
+print('next menggunakan while')
+

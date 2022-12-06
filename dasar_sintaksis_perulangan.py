a48764c4d14745586ed6cb3f4859d6d3402f2db5
Asus-Hasan <kejoramedia@gmail.com>
2022-12-06 21:46:39 +0700

    Sekuensial dan percabangan Sequential and branching

---
+++ dasar_sintaksis_perulangan.py
@@ -0,0 +1,12 @@
+"""
+Program Perulangan Membaca Buku
+"""
+jumlah_total_buku=10
+print('Ibu memberi perintah,"Baca semua bukumu" ')
+
+jumlah_buku_yg_sudah_dibaca=0
+print(f'Jumlah buku yg sudah dibaca {jumlah_buku_yg_sudah_dibaca}')
+
+for jumlah_buku_yg_sudah_dibaca in range (1, jumlah_total_buku+1):
+    print(f'Membaca buku ke {jumlah_buku_yg_sudah_dibaca}')
+print(f'Jumlah buku yg sudah dibaca {jumlah_buku_yg_sudah_dibaca}')

3584e97b847f64ca05101c49b79582bb0e68f087
Asus-Hasan <kejoramedia@gmail.com>
2022-12-08 21:07:20 +0700

    praktek sintaksis sekuensial penjumlahan 2 bilangan

---
+++ praktek_sintaksis_sekuensial.py
@@ -0,0 +1,14 @@
+"""
+# Praktek Sekuensial Operasi Penjumlahan 2 Bilangan
+"""
+print('Praktek Penjumlahan 2 Bilangan')
+
+x = 2
+print(f'x = {x}')
+
+y = 7
+print(f'y = {y}')
+
+hasil = x + y
+print(f'Hasil = {x} + {y}')
+print(f'Hasil = {hasil}')
